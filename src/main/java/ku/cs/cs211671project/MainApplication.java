7062a143a70e0641a505d32bd874614b4f30fe46
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-08-03 22:27:59 +0700

    Revert "edit size desktop"
    This reverts commit 4cec6b3ae64bec5e9a3fe8edc486f6f6379022cd.

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "CS211 Project", 1440, 1024);
+        FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
         configRoutes();
         FXRouter.goTo("first-page");
     }
