54aa17a1c0c6a9bf6b315ddb313d232238bf21e4
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-08-05 00:09:57 +0700

    my-team page

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -11,15 +11,15 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
+        FXRouter.bind(this, stage, "CS211 Project", 1280, 720);
         configRoutes();
-        FXRouter.goTo("first-page");
+        FXRouter.goTo("my-team");
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
         FXRouter.when("hello", viewPath + "hello-view.fxml");
-        FXRouter.when("first-page", viewPath + "first-page.fxml");
+        FXRouter.when("my-team", viewPath + "my-team.fxml");
     }
 
     public static void main(String[] args) {
