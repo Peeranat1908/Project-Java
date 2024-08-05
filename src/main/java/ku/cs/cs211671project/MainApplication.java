b0c50c587ed15a2edcf62921d5c86aaa208837a9
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-08-05 20:23:12 +0700

    add login-page v2 by nick

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -13,21 +13,15 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
         configRoutes();
-<<<<<<< HEAD
         FXRouter.goTo("first-page");
-=======
         FXRouter.goTo("loginpage");
->>>>>>> feature/Login-Page
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
         FXRouter.when("hello", viewPath + "hello-view.fxml");
-<<<<<<< HEAD
         FXRouter.when("first-page", viewPath + "first-page.fxml");
-=======
         FXRouter.when("loginpage", viewPath + "login.fxml");
->>>>>>> feature/Login-Page
     }
 
     public static void main(String[] args) {
