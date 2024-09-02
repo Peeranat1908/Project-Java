b997ab70f920c99b1e3ff01220d77770d63606cb
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-09-02 18:07:53 +0700

    edit1

---
+++ src/main/java/ku/cs/models/StudentAdvisor.java
@@ -0,0 +1,16 @@
+package ku.cs.models;
+
+public class StudentAdvisor {
+    private String id;
+    private String name;
+    private String faculty;
+    private String major;
+
+    public StudentAdvisor(String id, String name, String faculty, String major){
+        this.id = id;
+        this.name = name;
+        this.faculty = faculty;
+        this.major = major;
+    }
+
+}
