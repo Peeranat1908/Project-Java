cfae8ddae57c4f8da820313fe21219007602766d
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-09-23 20:57:46 +0700

    edit main

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -13,9 +13,10 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "CS211 Project", 1280, 720);
         configRoutes();
-        FXRouter.goTo("first-page");
+        //FXRouter.goTo("first-page");
         //FXRouter.goTo("main-advisor");
         //FXRouter.goTo("departmentStaff");
+        FXRouter.goTo("facultyStaff");
 
     }
 
