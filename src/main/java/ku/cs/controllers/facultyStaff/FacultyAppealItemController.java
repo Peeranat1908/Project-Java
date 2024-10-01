e66a78d70d40ca51681cb6462fe784481edf5d2d
Peeranat1908 <145495460+Peeranat1908@users.noreply.github.com>
2024-10-01 21:43:10 +0700

    add detail page

---
+++ src/main/java/ku/cs/controllers/facultyStaff/FacultyAppealItemController.java
@@ -0,0 +1,118 @@
+package ku.cs.controllers.facultyStaff;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import ku.cs.models.Appeal;
+import ku.cs.models.AppealList;
+import ku.cs.models.User;
+import ku.cs.services.AppealListDatasource;
+import ku.cs.services.AppealSharedData;
+import ku.cs.services.FXRouter;
+
+import java.io.IOException;
+
+public class FacultyAppealItemController {
+    @FXML
+    private Label typeLabel;
+    @FXML
+    private Label subjectLabel;
+    @FXML
+    private Label requestLabel;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private Label signatureLabel;
+    @FXML
+    private Button resetDecline;
+    @FXML
+    private Button applyDecline;
+
+    @FXML
+    private TextField declineTextField;
+
+
+    private User user;
+
+    @FXML
+    public void initialize() {
+        Appeal appeal = AppealSharedData.getSelectedAppeal();
+        if (appeal != null) {
+            typeLabel.setText(appeal.getType());
+            subjectLabel.setText(appeal.getSubject());
+            requestLabel.setText(appeal.getRequest());
+            dateLabel.setText(appeal.getDate().toString());
+            signatureLabel.setText(appeal.getStudentSignature());
+        }
+
+        Object data = FXRouter.getData();
+        if (data instanceof User) {
+            user = (User) data;
+        }
+    }
+
+    @FXML
+    public void onApproveAppealClick(){
+        Appeal appeal = AppealSharedData.getSelectedAppeal();
+        if(appeal != null){
+            appeal.setStatus("อนุมัติโดยอาจารย์ที่ปรึกษา คำร้องส่งต่อให้หัวหน้าภาควิชา");
+        }
+        AppealListDatasource datasource = new AppealListDatasource("data/appeals.csv");
+        AppealList appealList = AppealSharedData.getNormalAppealList();
+
+        datasource.writeData(appealList);
+        try {
+            FXRouter.goTo("advisor-appeal-page", user);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void onDeclineAppealClick(){
+        declineTextField.setVisible(true);
+        resetDecline.setVisible(true);
+        applyDecline.setVisible(true);
+    }
+
+    @FXML
+    public void DeclineApplyClick(){
+        String DeclineReason = declineTextField.getText();
+
+        Appeal appeal = AppealSharedData.getSelectedAppeal();
+        if(appeal != null){
+            appeal.setDeclineReason(DeclineReason);
+            appeal.setStatus("ปฏิเสธโดยอาจารย์ที่ปรึกษา คำร้องถูกปฏิเสธ");
+        }
+        AppealListDatasource datasource = new AppealListDatasource("data/appeals.csv");
+        AppealList appealList = AppealSharedData.getNormalAppealList();
+
+        datasource.writeData(appealList);
+        try {
+            FXRouter.goTo("advisor-appeal-page", user);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    public void onResetDeclineClick(){
+        clearFields();
+    }
+
+    private void clearFields() {
+        declineTextField.clear();
+    }
+
+
+
+
+    @FXML
+    public void onBackButtonClick() {
+        try {
+            FXRouter.goTo("advisor-appeal-page", user);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
